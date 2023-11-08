74d3d17a71c1bcf20c92d53411a73621a15c42c1
williamshdsilva <111313533+williamshdsilva@users.noreply.github.com>
2023-11-08 09:08:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function helloWorld() {
+    console.log("Hello, World!");
+  }
+  
+  function greet(name) {
+    console.log(`Hello, ${name}!`);
+  }
+  
+  const double = (x) => x * 2;
+  helloWorld();
+  greet("Jose");
+  console.log(double(5));
+  
